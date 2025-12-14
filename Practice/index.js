7f247f0c6a9c0297400bce2624406675af4f3c11
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-14 21:36:54 +0530

    Add the Practice Folders, with the the music .mp3 files, and also teh custom sleep function, and also teh volume icon feature and also teh index file, with the just the practice folder for practice before implementing on the react project

---
+++ Practice/index.js
@@ -0,0 +1,20 @@
+import movieList from "../src/music-list.js";
+ let movieName = [
+      {name: "A Mai Re new Kuma Sagar", file: "Practice/music-1.mp3"},
+      {name: "BHAWANA  Rockheads  Official Music Video - Rockheads Nepal", file: "Practice/music-2.mp3"},
+      {name: "Bidhan Shrestha - Yo Naniko Siraima  New Nepali Adhunik Song  Aahana Basnet & Bidhyanchal Bista", file: "Practice/music-3.mp3"}
+    ]
+
+let nn="new".toLowerCase();
+let storeAllMovie=movieName.filter(f=>{
+  let name=(f.name).toLowerCase()
+  return name.includes(nn)
+})
+
+// console.log(storeAllMovie);
+
+
+let value=20;
+setTimeout(() => {
+  console.log(value);
+}, 7000);
